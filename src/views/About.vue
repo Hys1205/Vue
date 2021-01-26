bf9c2ebb378a33638a0d121a5c24067804513fdd
hjk <12454845@qq.com>
2021-01-26 15:41:30 +0800

    学习vue基础

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+	<div>
+		<h2>点击案例用来加减数</h2>
+		<p>
+			<button @click="reduce">-</button>
+			<span>{{num}}</span>
+			<button @click="add">+</button>
+		</p>
+	</div>
   </div>
 </template>
+<script>
+	export default {
+		data(){
+			return{
+				num:0
+			}
+		},
+		methods:{
+			reduce(){
+				this.num--;
+			},
+			add(){
+				this.num++;
+			}
+		}
+	}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+	<img alt="Vue logo" src="../assets/u.jpg">
+	<!-- <img alt="Vue logo" src="../assets/sr.gif"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
